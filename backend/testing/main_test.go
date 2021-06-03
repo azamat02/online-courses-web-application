bb0d1e2a405f0afcb77a2a7e57535e366a58748e
azamat02 <azamattolegenov1@gmail.com>
2021-06-03 10:12:33 +0600

    Added testing directory, with one file main_test, to test running main application

--- backend/testing/main_test.go
+++ backend/testing/main_test.go
@@ -1,13 +1,14 @@
-package main
+package testing
 
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"online-courses-app/database"
 	"online-courses-app/routes"
+	"testing"
 )
 
-func main() {
+func TestMain (m *testing.M) {
 	//DB connect
 	database.Connect()
 
@@ -24,5 +25,3 @@ func main() {
 	//Running server
 	app.Listen(":8000")
 }
-
-
